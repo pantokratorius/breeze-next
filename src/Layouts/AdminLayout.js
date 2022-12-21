1b05d101ced192edd2f616a2e5b396a7619b7162
Erik <eitu@mail.ru>
2022-12-21 15:36:00 +0300

    imported files

---
+++ src/Layouts/AdminLayout.js
@@ -0,0 +1,202 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import Head from 'next/head'
+
+const AdminLayout = () => {
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Admin Panel
+                </h2>
+            }>
+
+            <Head>
+                <title>Laravel - Dashboard</title>
+            </Head>
+
+            <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
+      </li>
+      <li className="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" className="nav-link">Home</a>
+      </li>
+      <li className="nav-item d-none d-sm-inline-block">
+        <a href="#" className="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <a className="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i className="fas fa-search"></i>
+        </a>
+        <div className="navbar-search-block">
+          <form className="form-inline">
+            <div className="input-group input-group-sm">
+              <input className="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search"/>
+              <div className="input-group-append">
+                <button className="btn btn-navbar" type="submit">
+                  <i className="fas fa-search"></i>
+                </button>
+                <button className="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+
+      {/* <li className="nav-item dropdown">
+        <a className="nav-link" data-toggle="dropdown" href="#">
+          <i className="far fa-comments"></i>
+          <span className="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" className="dropdown-item">
+            <div className="media">
+              <img src="../dist/img/user1-128x128.jpg" alt="User Avatar" className="img-size-50 mr-3 img-circle"/>
+              <div className="media-body">
+                <h3 className="dropdown-item-title">
+                  Brad Diesel
+                  <span className="float-right text-sm text-danger"><i className="fas fa-star"></i></span>
+                </h3>
+                <p className="text-sm">Call me whenever you can...</p>
+                <p className="text-sm text-muted"><i className="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item">
+            <div className="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" className="img-size-50 img-circle mr-3"/>
+              <div className="media-body">
+                <h3 className="dropdown-item-title">
+                  John Pierce
+                  <span className="float-right text-sm text-muted"><i className="fas fa-star"></i></span>
+                </h3>
+                <p className="text-sm">I got your message bro</p>
+                <p className="text-sm text-muted"><i className="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item">
+            <div className="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" className="img-size-50 img-circle mr-3"/>
+              <div className="media-body">
+                <h3 className="dropdown-item-title">
+                  Nora Silvester
+                  <span className="float-right text-sm text-warning"><i className="fas fa-star"></i></span>
+                </h3>
+                <p className="text-sm">The subject goes here</p>
+                <p className="text-sm text-muted"><i className="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li> */}
+      {/* <li className="nav-item dropdown">
+        <a className="nav-link" data-toggle="dropdown" href="#">
+          <i className="far fa-bell"></i>
+          <span className="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span className="dropdown-item dropdown-header">15 Notifications</span>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item">
+            <i className="fas fa-envelope mr-2"></i> 4 new messages
+            <span className="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item">
+            <i className="fas fa-users mr-2"></i> 8 friend requests
+            <span className="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item">
+            <i className="fas fa-file mr-2"></i> 3 new reports
+            <span className="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div className="dropdown-divider"></div>
+          <a href="#" className="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li> */}
+      {/* <li className="nav-item">
+        <a className="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i className="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li> */}
+      {/* <li className="nav-item">
+        <a className="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+          <i className="fas fa-th-large"></i>
+        </a>
+      </li> */}
+    </ul>
+  </nav>
+  <aside className="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="index3.html" className="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" />
+      <span className="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <div className="sidebar">
+      <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div className="image">
+          <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image"/>
+        </div>
+        <div className="info">
+          <a href="#" className="d-block">Vaidotas Valancius</a>
+        </div>
+      </div>
+
+      <div className="form-inline">
+        <div className="input-group" data-widget="sidebar-search">
+          <input className="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search"/>
+          <div className="input-group-append">
+            <button className="btn btn-sidebar">
+              <i className="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <nav className="mt-2">
+        <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              <i className="nav-icon fas fa-book"></i>
+              <p>
+                Pages
+                <i className="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul className="nav nav-treeview" style={{display:'block'}}>
+              <li className="nav-item">
+                <a href="../examples/invoice.html" className="nav-link">
+                  <i className="far fa-circle nav-icon !text-[10px]"></i>
+                  <p>All Pages</p>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="../examples/invoice.html" className="nav-link">
+                  <i className="far fa-circle nav-icon !text-[10px]"></i>
+                  <p>Add New</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
+            
+        </AppLayout>
+    )
+}
+
+export default AdminLayout
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,10 @@ class MyDocument extends Document {
                         href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"
                         rel="stylesheet"
                     />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"/>
+                    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"/>
+                    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
+                    <link rel="stylesheet" href="dist/css/adminlte.min.css"/>
                 </Head>
                 <body className="antialiased">
                     <Main />
